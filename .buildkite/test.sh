91731ae3e623d5a81aed2af0ec54e2adcc86d82d
shauryachats <shauryachats@uber.com>
2026-02-06 21:01:20 +0000

    [1/n][uber-internal] Add arcconfig, METADATA and other internal files
    Summary:
    Base commit that adds Uber-related files to enable internal builds, diff reviews, etc.
    
    Will be merged on `early-release-1.5.x`.
    
    NOTE: In 1.5, maven version was upgraded from 35 to 37, which requires Maven 3.9+. This breaks our release.sh pipelines which is non-trivial to fix (GPG issues, unexpected tests running), and hence this build still uses Maven 3.8. The upgrade to Maven 3.9 is kept for a future subversion release.
    
    Test Plan: Test details here: https://docs.google.com/document/d/1TNKjp64DpUTyv70pDuQ-QZlYSMyHX6JUwNnvV_aOCNI/edit?tab=t.diddsvxomfia
    
    Reviewers: #ldap_rta-group, ureview, tarunm
    
    Reviewed By: #ldap_rta-group, tarunm
    
    Subscribers: xuanyili
    
    JIRA Issues: EVA-11543
    
    Differential Revision: https://code.uberinternal.com/D21590529

---
+++ .buildkite/test.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+set -ex
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+# Setup the JAVA path
+setup_env () {
+    export JAVA_HOME=$JAVA_HOME_PATH
+    export PATH=$JAVA_HOME/bin:$PATH
+    echo java --version
+}
+
+# Download the JAVA 11
+setup_java () {
+	rm -rf $JAVA_HOME_PATH || true
+    if [[ ! -d $JAVA_HOME_PATH ]]; then
+        mkdir -p $JAVA_HOME_PATH
+    fi
+
+    BINARY_ARCHIVE_NAME="$(echo $JAVA_JDK_NUMBER | sed 's/-//g').tar.gz"
+    ARCHIVE_INSTALL_PATH="$JAVA_HOME_PATH/$BINARY_ARCHIVE_NAME"
+    DOWNLOAD_SUCCEEDED=false
+
+    wget "http://artifactory.uber.internal:4587/artifactory/libs-release-local/com/uber/devxp/jdk-linux/${JAVA_JDK_NUMBER}/jdk-linux-${JAVA_JDK_NUMBER}.tar.gz" -P ${ARCHIVE_INSTALL_PATH}
+    if [[ "$?" -eq 0 ]]; then
+        DOWNLOAD_SUCCEEDED=true
+    fi
+
+    if [[ $DOWNLOAD_SUCCEEDED == "false" ]]; then
+        echo "Unable to download JDK $JAVA_JDK_NUMBER"
+        return
+    else
+        echo "Download JDK $JAVA_JDK_NUMBER succeeded"
+    fi
+    tar -xvzf $ARCHIVE_INSTALL_PATH/*.tar.gz -C $JAVA_HOME_PATH --strip 1 1>/dev/null 2>&1
+}
+
+JAVA_JDK_NUMBER="11.0.11_9"
+JAVA_HOME_PATH="$HOME/java_home/$JAVA_JDK_NUMBER"
+
+if [[ -f "$JAVA_HOME_PATH/bin/java" ]]; then
+    echo "JDK 11 was already downloaded"
+else
+    echo "JDK 11 will be downloaded"
+    setup_java
+fi
+setup_env
+
+TOKEN=$(echo -n "ignored:$UNPM_TOKEN" | base64 -w 0)
+echo -e "\n" >> ~/.npmrc
+echo "//unpm.uberinternal.com/:_auth=$TOKEN" >> ~/.npmrc
+
+mvn install -DskipTests -Dlicense.skip=true -Drat.ignoreErrors=true -Pbin-dist -T 1C
+mvn test -pl 'pinot-spi,pinot-clients,pinot-plugins,pinot-connectors,pinot-server,pinot-minion,pinot-broker,pinot-distribution,pinot-segment-spi,pinot-tools,pinot-perf,pinot-integration-test-base,pinot-compatibility-verifier,pinot-query-planner,pinot-query-runtime' -Drat.ignoreErrors=true -T 1C
